8e13f046b14e6d364e30955bc6c4510dfc29ecb8
Eleonora Grigoryan <eleonora.grigoryan@outlook.com>
2018-07-28 18:39:58 +0400

    Final

--- maintain.sql
+++ maintain.sql
@@ -1,98 +1,32 @@
--- Auto Maintenance
+-- Auto Rebuild or Reorganize Based on Fragmentation Percent
 
-/* 
- * Select all the indexes in a specified database
- *
- * select 
- *    idx.[name] as [Index]
- * from sys.indexes as idx
- * inner join sys.objects as obj on idx.object_id = obj.object_id
- * where idx.[name] is not null and obj.[type] = 'u'
- * order by idx.[name]
- */
- 
-/*
- * Print more information about chosen indexes
- *
- * select 
- *    db_name() as [Database],
- *    sc.[name] as [Schema],
- *    obj.[name] as [Table],  
- *    idx.[type] as [Index-Type],
- *  idx.[name] as [Index]
- * from sys.indexes as idx
- * inner join sys.objects as obj on idx.object_id = obj.object_id
- * inner join sys.schemas as sc  on obj.schema_id = sc.schema_id
- * where idx.[name] is not null and obj.[type] = 'u'
- * order by [Table]
- */
- 
-create table #temptable1
+create table #temptable
 (
- [database] sysname,
- [schema] nvarchar(40),
- [table] sysname,
- [index_type] nvarchar(40),
- [index_name] nvarchar(90),
- [object_id] int,
- [index_id] int
-)
-
-
-insert into #temptable1
-select
-	db_name() as [Database],
-	sc.[name] as [Schema],
-	obj.[name] as [Table],  
-	idx.[type_desc] as [Index Type],
-	idx.[name] as [Index Name],
-	idx.[object_id] as [Obj_id],
-	idx.[index_id] as [Ind_id]
-from sys.indexes as idx
- inner join sys.objects as obj on idx.object_id = obj.object_id
- inner join sys.schemas as sc  on obj.schema_id = sc.schema_id
-where idx.[name] is not null and obj.[type] = 'u' or obj.[type] = 'v' 
-
-
-/*
-
-select
-	db_name() as [Database],
-	sc.[name] as [Schema],
-	obj.[name] as [Table],  
-	idx.[type_desc] as [Index Type],
-	idx.[name] as [Index Name],
-	idx.[object_id] as [Obj_id],
-	ips.[avg_fragmentation_in_percent] as [Fragmentation Percent]
-from sys.indexes as idx
- inner join sys.objects as obj on idx.object_id = obj.object_id
- inner join sys.schemas as sc  on obj.schema_id = sc.schema_id
- left join sys.dm_db_index_physical_stats( NULL,NULL, NULL, NULL ,'LIMITED') AS ips on idx.[object_id] = ips.[object_id] and idx.index_id = ips.index_id 
-where database_id = db_id() and idx.[name] is not null and obj.[type] = 'u' or obj.[type] = 'v'
-ORDER BY [Table], idx.[type_desc]
-
-*/
-
-
-create table #temptable2
-(
+	[database_name] sysname,
+	[schema_name] sysname,
+	[table_name] sysname,
+	[index_type_description] nvarchar(20),
 	[index_name] sysname,
-	[fragmentation_percent] int
+	[partition_number] int,
+	[fragmentation_percent] int  
 )
 
-
-insert into #temptable2
+insert into #temptable
 select
-tb.[index_name],
-ips.[avg_fragmentation_in_percent] as [fragmentation_percent]
-from #temptable1 as tb
-left join sys.dm_db_index_physical_stats( NULL,NULL, NULL, NULL ,'LIMITED') as ips 
-on tb.[object_id] = ips.[object_id]  and tb.[index_id] = ips.index_id
-where database_id = db_id();
+    db_name() as [database_name],
+    sc.[name] as [schema_name],
+    obj.[name] as [table_name],  
+    idx.[type_desc] as [index_type_description],
+    idx.[name] as [index_name],
+    ips.partition_number as [partition_number],
+    ips.[avg_fragmentation_in_percent] as [fragmentation_percent]
+from sys.indexes as idx
+inner join sys.objects as obj on idx.object_id = obj.object_id
+inner join sys.schemas as sc  on obj.schema_id = sc.schema_id
+cross apply sys.dm_db_index_physical_stats( DB_ID(), idx.object_id, idx.index_id, NULL ,'LIMITED') AS ips
+where idx.[name] is not NULL
+order by [fragmentation_percent] desc;
 GO
-
---select * from #temptable2
---------------------------------------------
 
 alter procedure up_index_auto_maintenance
 
@@ -111,7 +45,7 @@ set @dynamic_reorganize = 'alter [' + @index_holder + '] on [' + db_name() +']  
 	declare cr_index cursor
 	
 	for 
-		select [index_name] from #temptable2 
+		select [index_name] from #temptable
 
 	open cr_index
 		
@@ -119,7 +53,7 @@ set @dynamic_reorganize = 'alter [' + @index_holder + '] on [' + db_name() +']  
 
 		while @@fetch_status = 0
 		begin
-			if (select [fragmentation_percent] from #temptable2 where [index_name] = @index_holder) > 30
+			if (select [fragmentation_percent] from #temptable where [index_name] = @index_holder) > 30
 			begin
 				exec(@dynamic_rebuild)
 				print @index_holder + 'is rebuilt'
@@ -140,3 +74,5 @@ set @dynamic_reorganize = 'alter [' + @index_holder + '] on [' + db_name() +']  
 end
 
 exec up_index_auto_maintenance
+
+
